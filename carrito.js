4529c10a766f690846e86e440f77d4e6cce32d3c
Natalia Cisneros <naticisneros.pipa@gmail.com>
2024-11-11 22:56:01 -0300

    reentrega

--- carrito.js
+++ carrito.js
@@ -2,32 +2,90 @@ const carritoResultado = JSON.parse(localStorage.getItem("carrito"));
 
 const contenedorCarrito = document.getElementById("CarritoDeCompras");
 
-carritoResultado.forEach(el =>{
+const borrarCarrito = document.getElementById("CarritoDeCompras");
+
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carritoResultado));
+}
+
+carritoResultado.forEach((el, index)=>{
     const articulo = document.createElement("div");
+    const imagen = document.createElement("img");
     const nombre = document.createElement("h3");
     const precio = document.createElement("p");
-    const id = document.createElement("p");
+    const botonMenos = document.createElement("button");
     const cantidad = document.createElement("p");
+    const botonMas = document.createElement("button");
+    const cantidadTotal = document.createElement("div");
+    const botonBorrarArticulo = document.createElement("button");
+    
+    articulo.className = "articuloCarrito";
+    cantidadTotal.className = "cantidadTotal";
 
-    articulo.className = "articulo";
+    botonMenos.addEventListener("click", () => {
+        if (el.cantidad > 1) { 
+            el.cantidad -= 1; 
+            cantidad.innerText = `Cantidad: ${el.cantidad}`;
+            calcularTotal();
+            guardarCarrito();
+        }
+    });
+
+    botonMas.addEventListener("click", () => {
+        el.cantidad += 1; 
+        cantidad.innerText = `Cantidad: ${el.cantidad}`;
+        calcularTotal();
+        guardarCarrito();
+    });
+
+    botonBorrarArticulo.addEventListener("click", () => {
+        carritoResultado.splice(index, 1);
+        contenedorCarrito.removeChild(articulo);
+        calcularTotal();
+        guardarCarrito();
+    });
 
     nombre.innerText = el.nombre;
     precio.innerText = `$${el.precio}`;
-    id.innerText = `ID: ${el.id}`;
+    botonMenos.innerText = "-";
+    botonMas.innerText = "+";
+    botonBorrarArticulo.innerText = "X";
     cantidad.innerText = `Cantidad: ${el.cantidad}`;
+    imagen.src = el.imagen;
+    imagen.alt = el.nombre;
 
+    articulo.appendChild(imagen);
     articulo.appendChild(nombre);
     articulo.appendChild(precio);
-    articulo.appendChild(id);
-    articulo.appendChild(cantidad);
+    cantidadTotal.appendChild(botonMenos);
+    cantidadTotal.appendChild(cantidad);
+    cantidadTotal.appendChild(botonMas);
+    articulo.appendChild(cantidadTotal);
+    articulo.appendChild(botonBorrarArticulo);
 
     contenedorCarrito.appendChild(articulo);
-
 })
 
-const contenedorTotal = document.getElementById("Total");
-const total = carritoResultado.reduce((acumulador, producto) => acumulador + (producto.precio * producto.cantidad)  ,0);
-contenedorTotal.textContent = `TOTAL: $${total}`;
+
+const botonBorrar = document.getElementById("borrarCarrito");
+botonBorrar.className = "botonBorrar"
+
+botonBorrar.addEventListener("click", () => {
+    carritoResultado.length = 0;
+    localStorage.setItem("carrito", JSON.stringify(carritoResultado));
+
+    const contenedorCarrito = document.getElementById("CarritoDeCompras");
+    contenedorCarrito.innerHTML = "";
+
+    const contenedorTotal = document.getElementById("Total");
+    contenedorTotal.textContent = "TOTAL: $0";
+});
 
 
+function calcularTotal() {
+    const contenedorTotal = document.getElementById("Total");
+    const total = carritoResultado.reduce((acumulador, producto) => acumulador + (producto.precio * producto.cantidad)  ,0);
+    contenedorTotal.textContent = `TOTAL: $${total}`;
+}
 
+calcularTotal();
--- main.js
+++ main.js
@@ -2,42 +2,58 @@ const articulos = [
     {
         id: 1, 
         nombre: "BERMUDA", 
-        precio: 25000
+        precio: 25000,
+        imagen: "./imagenes/bermuda.jpg",
+        cantidad: 1
     },
     {
         id: 2, 
         nombre: "PANTALON", 
-        precio: 70000
+        precio: 70000,
+        imagen: "./imagenes/pantalon.jpg",
+        cantidad: 1
     },
     {
         id: 3, 
         nombre: "REMERA", 
-        precio: 20000
+        precio: 20000,
+        imagen: "./imagenes/remera.jpg",
+        cantidad: 1
     },
     {
         id: 4, 
         nombre: "CAMPERA", 
-        precio: 80000
+        precio: 80000,
+        imagen: "./imagenes/campera.jpg",
+        cantidad: 1
     },
     {
         id: 5, 
         nombre: "CHALECO", 
-        precio: 45000
+        precio: 45000,
+        imagen: "./imagenes/chaleco.jpg",
+        cantidad: 1
     },
     {
         id: 6, 
         nombre: "GORRA", 
-        precio: 15000
+        precio: 15000,
+        imagen: "./imagenes/gorra.jpg",
+        cantidad: 1
     },
     {
         id: 7, 
         nombre: "CINTO", 
-        precio: 20000
+        precio: 20000,
+        imagen: "./imagenes/cinto.jpg",
+        cantidad: 1
     },
     {
         id: 8, 
         nombre: "BOLSO", 
-        precio: 50000
+        precio: 50000,
+        imagen: "./imagenes/bolso.jpg",
+        cantidad: 1
     }
 ]
 
@@ -49,12 +65,13 @@ const contenedorArticulos = document.getElementById("Tienda");
 
 articulos.forEach(el =>{
     const articulo = document.createElement("div");
+    const imagen = document.createElement("img");
     const nombre = document.createElement("h3");
     const precio = document.createElement("p");
-    const id = document.createElement("p");
+    
     const botonAgregarAlCarrito = document.createElement("button");
 
-    botonAgregarAlCarrito.className = "botonAgregar"
+    botonAgregarAlCarrito.className = "boton"
 
     botonAgregarAlCarrito.textContent = "Agregar al Carrito"
 
@@ -64,11 +81,14 @@ articulos.forEach(el =>{
 
     nombre.innerText = el.nombre;
     precio.innerText = `$${el.precio}`;
-    id.innerText = `ID: ${el.id}`;
+    imagen.src = el.imagen;
+    imagen.alt = el.nombre;
     
+    // Añadir los elementos al contenedor del artículo
+    articulo.appendChild(imagen);
     articulo.appendChild(nombre);
     articulo.appendChild(precio);
-    articulo.appendChild(id);
+    
     articulo.appendChild(botonAgregarAlCarrito);
 
     contenedorArticulos.appendChild(articulo);
